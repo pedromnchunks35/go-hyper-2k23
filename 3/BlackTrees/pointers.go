faeb3c27b149433e557af3bf5364095d44620e03
pedromnchunks35 <pedrosilvamnchunks@gmail.com>
2023-07-19 17:24:47 +0100

    Black tree done own impl

--- 3/BlackTrees/pointers.go
+++ 3/BlackTrees/pointers.go
@@ -8,6 +8,11 @@ type test struct {
 	Position2 *test
 }
 
+func (t *test) changePointers() {
+	h := &test{Value: 2, Position: nil}
+	t = h
+}
+
 func (t *test) changeValue() {
 	temp := (*t).Value
 	t.Value = (*t).Position.Value
@@ -25,12 +30,8 @@ func (t *test) tradeof() (result *test) {
 }
 
 func main() {
-	t2 := &test{Value: 3, Position: nil}
-	t := &test{Value: 2, Position: t2}
-	fmt.Printf("{Pos:%p,Value:%v},{Pos:%p,Value:%v}\n", t, t.Value, t2, t2.Value)
-	t = t.tradeof()
-	t2 = t.Position
-	fmt.Printf("{Pos:%p,Value:%v},{Pos:%p,Value:%v},{Pos: %p,Value: %v}\n", t, t.Value, t2, t2.Value, t.Position, t.Position.Value)
-	t.changeValue()
-	fmt.Printf("{Pos:%p,Value:%v},{Pos:%p,Value:%v},{Pos: %p,Value: %v}\n", t, t.Value, t2, t2.Value, t.Position, t.Position.Value)
+	t := &test{Value: 1}
+	fmt.Printf("%p %v\n", t, t)
+	t.changePointers()
+	fmt.Printf("%p %v\n", t, t)
 }
