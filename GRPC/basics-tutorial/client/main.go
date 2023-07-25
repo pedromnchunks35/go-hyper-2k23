e19a222c02a67ac6a4a5b9c7acae574b8c1b4a31
pedromnchunks35 <pedrosilvamnchunks@gmail.com>
2023-07-25 11:49:14 +0100

    Basic tutorial ok

--- GRPC/basics-tutorial/client/main.go
+++ GRPC/basics-tutorial/client/main.go
@@ -9,6 +9,11 @@ import (
 	"time"
 
 	pb "basic-tut/proto-files"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/examples/data"
 )
 
 // ? Init vars
@@ -63,6 +68,7 @@ func runRecordRoute(client pb.RouteGuideClient) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	pointCount := int(r.Int31n(100)) + 2
 	var points []*pb.Point
+	//? Add random point, just so see if that random point is inside of the range on the server side
 	for i := 0; i < pointCount; i++ {
 		points = append(points, randomPoint(r))
 	}
@@ -70,6 +76,7 @@ func runRecordRoute(client pb.RouteGuideClient) {
 	//? create some context
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	//? Get the stream, using a certain context
 	stream, err := client.RecordRoute(ctx)
 	if err != nil {
 		log.Fatalf("client.RecordRoute failed: %v", err)
@@ -87,4 +94,100 @@ func runRecordRoute(client pb.RouteGuideClient) {
 	log.Printf("Route summary: %v", reply)
 }
 
-//? runRouteChat receives
+// ? runRouteChat receives a sequence of route notes, while sending notes for various locations
+func runRouteChat(client pb.RouteGuideClient) {
+	//? Writing down the notes
+	notes := []*pb.RouteNote{
+		{Location: &pb.Point{Latitude: 0, Longitude: 1}, Message: "First message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 2}, Message: "Second message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 3}, Message: "Third message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 1}, Message: "Fourth message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 2}, Message: "Fifth message"},
+		{Location: &pb.Point{Latitude: 0, Longitude: 3}, Message: "Sixth message"},
+	}
+	//? Create some context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//? Cancel the operation, it is meant for releasing resources
+	defer cancel()
+	//? Call the method from the interface
+	stream, err := client.RouteChat(ctx)
+	if err != nil {
+		log.Fatalf("client.RouteChat failed: %v", err)
+	}
+	//? Create channel of structs
+	waitc := make(chan struct{})
+	//? Init a goroutine
+	go func() {
+		for {
+			//? Make the next read
+			in, err := stream.Recv()
+			if err == io.EOF {
+				//? The read now is done, it is used to signal the end of the reading,this way the function will still be up
+				//? until this goroutine ends
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("client.RouteChat failed: %v", err)
+			}
+			//? Get the message
+			log.Printf("Got message %s at point(%d,%d)", in.Message, in.Location.Latitude, in.Location.Longitude)
+		}
+	}()
+	//? Range the notes
+	for _, note := range notes {
+		if err := stream.Send(note); err != nil {
+			log.Fatalf("client.RouteChat: stream.Send(%v) failed: %v", note, err)
+		}
+	}
+	stream.CloseSend()
+	//? Receiving of the signal
+	<-waitc
+}
+
+func randomPoint(r *rand.Rand) *pb.Point {
+	lat := (r.Int31n(180) - 90) * 1e7
+	long := (r.Int31n(360) - 180) * 1e7
+	return &pb.Point{Latitude: lat, Longitude: long}
+}
+
+func main() {
+	//? Get the flags
+	flag.Parse()
+	//? initiate the opts
+	var opts []grpc.DialOption
+	//? get the tls binding opt
+	if *tls {
+		if *caFile == "" {
+			*caFile = data.Path("x509/ca_cert.pem")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials: %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+	//? Create a connection
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	//? On the final of the other activites we need to close the connection
+	defer conn.Close()
+	client := pb.NewRouteGuideClient(conn)
+	//? Looking for a valid feature
+	printFeature(client, &pb.Point{Latitude: 409146138, Longitude: -746188906})
+	//? Feature missing.
+	printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
+	//? Looking for features between 40,-75 and 42,-73
+	printFeatures(client, &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000},
+		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
+	})
+	//? Record the route
+	runRecordRoute(client)
+	//? Route chat
+	runRouteChat(client)
+}
